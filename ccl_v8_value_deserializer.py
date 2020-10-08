231de183b50a32887bee01c946409b73cce0643f
Alex Caithness <acaithness@cclgroupltd.com>
2020-10-08 12:15:26 +0100

    remove some imports and add licenses

--- ccl_v8_value_deserializer.py
+++ ccl_v8_value_deserializer.py
@@ -1,3 +1,25 @@
+"""
+Copyright 2020, CCL Forensics
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 import sys
 import struct
 import datetime
@@ -244,21 +266,6 @@ class Deserializer:
 
     def _read_le_varint(self) -> typing.Optional[typing.Tuple[int, bytes]]:
         return read_le_varint(self._f)
-        # # this only outputs unsigned
-        # i = 0
-        # result = 0
-        # underlying_bytes = []
-        # while i < 10:  # 64 bit max possible?
-        #     raw = self._f.read(1)
-        #     if len(raw) < 1:
-        #         return None
-        #     tmp, = raw
-        #     underlying_bytes.append(tmp)
-        #     result |= ((tmp & 0x7f) << (i * 7))
-        #     if (tmp & 0x80) == 0:
-        #         break
-        #     i += 1
-        # return result, bytes(underlying_bytes)
 
     def _read_zigzag(self) -> int:
         unsigned = self._read_le_varint()[0]
