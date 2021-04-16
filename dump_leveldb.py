8c0416e9d96025c4c93362fd10dd3c90b4697ce3
Alex Caithness <acaithness@cclgroupltd.com>
2021-04-16 17:23:42 +0100

    added main function and usage

--- dump_leveldb.py
+++ dump_leveldb.py
@@ -1,26 +1,36 @@
-import ccl_leveldb
-import csv
 import sys
+import csv
+import ccl_leveldb
+import pathlib
 
-input_path = sys.argv[1]
-output_path = "leveldb_dump.csv"
-if len(sys.argv) > 2:
-    output_path = sys.argv[2]
 
-leveldb_records = ccl_leveldb.RawLevelDb(input_path)
+def main(args):
+    input_path = args[0]
+    output_path = "leveldb_dump.csv"
+    if len(args) > 1:
+        output_path = args[1]
 
-with open(output_path, "w", newline="") as file1:
-    writes = csv.writer(file1, quoting=csv.QUOTE_ALL)
-    writes.writerow(
-        ["key", "value", "origin_file", "file_type", "offset", "seq", "state", "was_compressed"])
-    for record in leveldb_records.iterate_records_raw():
-        writes.writerow([
-            record.key,
-            record.value,
-            str(record.origin_file),
-            record.file_type.name,
-            record.offset,
-            record.seq,
-            record.state.name,
-            record.was_compressed
-        ])
+    leveldb_records = ccl_leveldb.RawLevelDb(input_path)
+
+    with open(output_path, "w", newline="") as file1:
+        writes = csv.writer(file1, quoting=csv.QUOTE_ALL)
+        writes.writerow(
+            ["key", "value", "origin_file", "file_type", "offset", "seq", "state", "was_compressed"])
+        for record in leveldb_records.iterate_records_raw():
+            writes.writerow([
+                record.key,
+                record.value,
+                str(record.origin_file),
+                record.file_type.name,
+                record.offset,
+                record.seq,
+                record.state.name,
+                record.was_compressed
+            ])
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print(f"Usage: {pathlib.Path(sys.argv[0]).name} <in dir path> [outpath.csv]")
+        exit(1)
+    main(sys.argv[1:])
