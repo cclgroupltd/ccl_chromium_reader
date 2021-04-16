8d11dd3a4d98fa1fc7ea3fd356403cd194c166d3
Alex Caithness <acaithness@cclgroupltd.com>
2021-04-16 17:49:44 +0100

    added separate text and hex previews of keys and values. Added warning regarding encoding

--- dump_leveldb.py
+++ dump_leveldb.py
@@ -3,6 +3,8 @@ import csv
 import ccl_leveldb
 import pathlib
 
+ENCODING = "iso-8859-1"
+
 
 def main(args):
     input_path = args[0]
@@ -12,14 +14,20 @@ def main(args):
 
     leveldb_records = ccl_leveldb.RawLevelDb(input_path)
 
-    with open(output_path, "w", newline="") as file1:
+    with open(output_path, "w", encoding="utf-8", newline="") as file1:
         writes = csv.writer(file1, quoting=csv.QUOTE_ALL)
         writes.writerow(
-            ["key", "value", "origin_file", "file_type", "offset", "seq", "state", "was_compressed"])
+            [
+                "key-hex", "key-text", "value-hex", "value-text", "origin_file",
+                "file_type", "offset", "seq", "state", "was_compressed"
+            ])
+
         for record in leveldb_records.iterate_records_raw():
             writes.writerow([
-                record.key,
-                record.value,
+                record.key.hex(" ", 1),
+                record.key.decode(ENCODING, "replace"),
+                record.value.hex(" ", 1),
+                record.value.decode(ENCODING, "replace"),
                 str(record.origin_file),
                 record.file_type.name,
                 record.offset,
@@ -31,6 +39,15 @@ def main(args):
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        print(f"Usage: {pathlib.Path(sys.argv[0]).name} <in dir path> [outpath.csv]")
+        print(f"Usage: {pathlib.Path(sys.argv[0]).name} <indir path> [outpath.csv]")
         exit(1)
+    print()
+    print("+--------------------------------------------------------+")
+    print("|Please note: keys and values in leveldb are binary blobs|")
+    print("|so any text seen in the output of this script might not |")
+    print("|represent the entire meaning of the data. The output of |")
+    print("|this script should be considered as a preview of the    |")
+    print("|data only.                                              |")
+    print("+--------------------------------------------------------+")
+    print()
     main(sys.argv[1:])
